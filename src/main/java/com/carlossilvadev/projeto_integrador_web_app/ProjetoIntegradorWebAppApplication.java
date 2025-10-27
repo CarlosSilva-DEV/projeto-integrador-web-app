759cdbc3cd2ae47564b711df4aa50343cbf6c853
Carlos Silva <cd160204@gmail.com>
2025-10-27 19:28:42 -0300

    Spring Boot project created

---
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/ProjetoIntegradorWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.carlossilvadev.projeto_integrador_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlossilvadev/projeto_integrador_web_app/ProjetoIntegradorWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlossilvadev.projeto_integrador_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
