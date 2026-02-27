775ccf984e3f822c51072e3c2734f810d07c1313
Carlos Silva <cd160204@gmail.com>
2026-02-26 23:26:50 -0300

    TestDevProdConfig
    Atualizando classe de configs para incluir perfil prod e testar instanciamento de objetos no deploy

--- src/main/java/com/carlossilvadev/projeto_integrador_web_app/config/TestDevProdConfig.java
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/config/TestDevProdConfig.java
@@ -12,8 +12,8 @@ import com.carlossilvadev.projeto_integrador_web_app.repositories.CategoryReposi
 import com.carlossilvadev.projeto_integrador_web_app.repositories.ProductRepository;
 
 @Configuration
-@Profile({"test", "dev"}) // define como uma classe de configuração, especifica pros perfis "test" e "dev"
-public class TestDevConfig implements CommandLineRunner{
+@Profile({"test", "dev", "prod"}) // define como uma classe de configuração, especifica pros perfis "test" e "dev"
+public class TestDevProdConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
