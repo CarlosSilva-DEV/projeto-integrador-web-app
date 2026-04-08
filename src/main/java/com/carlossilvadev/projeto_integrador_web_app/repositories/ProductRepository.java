4f48d6c48cd90683bb408d7b01614da6fc55b0a0
Carlos Silva <cd160204@gmail.com>
2026-04-07 22:28:09 -0300

    refactor: remove métodos não utilizados em ProductRepository
    este commit refatora a interface ProductRepository, removendo métodos não utilizados e não relevantes para o funcionamento da aplicação.

--- src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/ProductRepository.java
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/ProductRepository.java
@@ -22,15 +22,11 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
 	
 	// busca de produtos por nome
 	List<Product> findByNomeContainingIgnoreCase(String nome);
-	List<Product> findByNomeContaining(String nome);
 	
 	
 	// filtros para busca
 	@Query("SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.categories c WHERE c.id = :categoryId")
 	List<Product> findByCategoryId(@Param("categoryId") Long categoryId);
-	
-	@Query("SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.categories c WHERE c.id IN :categoryIds")
-	List<Product> findByCategoryIds(@Param("categoryIds") List<Long> categoryIds);
 	
 	@Query("SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.categories c WHERE " +
 	           "(:categoryId IS NULL OR c.id = :categoryId) AND " +
