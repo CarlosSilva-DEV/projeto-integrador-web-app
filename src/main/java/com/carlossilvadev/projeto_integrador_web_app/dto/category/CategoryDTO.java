51c16691abca7c828dd4dd115208fd50ffe73e19
Carlos Silva <cd160204@gmail.com>
2026-04-14 23:33:09 -0300

    refactor: remove campo products, setters e atribuição com copyProperties de CategoryDTO
    este commit refatora CategoryDTO, removendo o Set de produtos, evitando bug de loop de associações no Construtor (new ProductDTO -> new CategoryDTO -> new ProductDTO). além disso, remove setters não utilizados e remove o método copyProperties para alterar a lógica de atribuição de campos do Construtor, garantindo leve ganho de performance, imutabilidade e consistência de dados

--- src/main/java/com/carlossilvadev/projeto_integrador_web_app/dto/category/CategoryDTO.java
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/dto/category/CategoryDTO.java
@@ -1,12 +1,6 @@
 package com.carlossilvadev.projeto_integrador_web_app.dto.category;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.springframework.beans.BeanUtils;
-
 import com.carlossilvadev.projeto_integrador_web_app.entities.Category;
-import com.carlossilvadev.projeto_integrador_web_app.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -18,36 +12,21 @@ public class CategoryDTO {
 	@Size(min = 2, max = 20, message = "Nome deve ter entre 2 a 20 caracteres")
 	private String nome;
 	
-	private Set<Product> products = new HashSet<>();
-	
 	// Construtores
 	public CategoryDTO(Category category) {
-		BeanUtils.copyProperties(category, this);
-		
-		if (category.getProducts() != null) {
-			this.products = new HashSet<>(category.getProducts());
-		}
+		this.id = category.getId();
+		this.nome = category.getNome();
 	}
 	
 	public CategoryDTO() {
 	}
 	
-	// getters e setters
+	// getters
 	public Long getId() {
 		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getNome() {
 		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Set<Product> getProducts() {
-		return products;
 	}
 }
