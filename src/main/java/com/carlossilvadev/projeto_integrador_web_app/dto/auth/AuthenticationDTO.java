b401d9e1eabbb082e964d51f17c84061ab5f1ce3
Carlos Silva <cd160204@gmail.com>
2026-04-14 23:01:06 -0300

    refactor: remove setters de AuthenticationDTO
    este commit remove setters não utilizados da classe AuthenticationDTO, garantindo imutabilidade do DTO

--- src/main/java/com/carlossilvadev/projeto_integrador_web_app/dto/auth/AuthenticationDTO.java
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/dto/auth/AuthenticationDTO.java
@@ -14,18 +14,12 @@ public class AuthenticationDTO {
 	@JsonProperty("senha")
 	private String password;
 	
-	// getters e setters
+	// getters
 	public String getUsername() {
 		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
 	}
 	
 	public String getPassword() {
 		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 }
