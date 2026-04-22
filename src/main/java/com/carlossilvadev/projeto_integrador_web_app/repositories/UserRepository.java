33844ce4c149993d8b262b9099f55262a71d4636
Carlos Silva <cd160204@gmail.com>
2026-04-22 17:30:40 -0300

    feat: cria métodos findByIdWithOrders e findAllWithOrders em UserRepository
    este commit cria os métodos findByIdWithOrders e findAllWithOrders em UserRepository, realizando JOIN FETCH com orders, items e products. estes métodos substituem os métodos default JPA (findById e findAll) para prevenir LazyInitializationException

--- src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/UserRepository.java
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.carlossilvadev.projeto_integrador_web_app.repositories;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.carlossilvadev.projeto_integrador_web_app.entities.User;
@@ -11,4 +14,10 @@ public interface UserRepository extends JpaRepository<User, Long>{ // extende jp
 	
 	Optional<User> findByLogin(String login);
 	Optional<User> findByEmail(String email);
+	
+	@Query("SELECT u FROM User u LEFT JOIN FETCH u.orders o LEFT JOIN FETCH o.items i LEFT JOIN FETCH i.id.product WHERE u.id = :id")
+	Optional<User> findByIdWithOrders(@Param("id") Long id);
+	
+	@Query("SELECT u FROM User u LEFT JOIN FETCH u.orders o LEFT JOIN FETCH o.items i LEFT JOIN FETCH i.id.product")
+	List<User> findAllWithOrders();
 }
