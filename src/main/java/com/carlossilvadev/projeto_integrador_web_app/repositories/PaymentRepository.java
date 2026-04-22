382ed2b0074d0848284759a40ae974d97ccc64f2
Carlos Silva <cd160204@gmail.com>
2026-04-22 16:26:48 -0300

    perf: altera métodos de busca de PaymentRepository
    este commit adiciona JOIN FETCH com items e products aos métodos findAllWithOrdersAndClients e findByIdWithOrdersAndClients de PaymentRepository, melhorando a performance de busca e prevenindo LazyInitializationException

--- src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/PaymentRepository.java
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/PaymentRepository.java
@@ -10,9 +10,9 @@ import org.springframework.data.repository.query.Param;
 import com.carlossilvadev.projeto_integrador_web_app.entities.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long>{
-	@Query("SELECT DISTINCT p FROM Payment p LEFT JOIN FETCH p.order o LEFT JOIN FETCH o.client WHERE p.order IS NOT NULL")
+	@Query("SELECT DISTINCT p FROM Payment p LEFT JOIN FETCH p.order o LEFT JOIN FETCH o.client LEFT JOIN FETCH o.items i LEFT JOIN FETCH i.id.product WHERE p.order IS NOT NULL")
 	List<Payment> findAllWithOrdersAndClients();
 	
-	@Query("SELECT p FROM Payment p LEFT JOIN FETCH p.order o LEFT JOIN FETCH o.client WHERE p.id = :id")
+	@Query("SELECT p FROM Payment p LEFT JOIN FETCH p.order o LEFT JOIN FETCH o.client LEFT JOIN FETCH o.items i LEFT JOIN FETCH i.id.product WHERE p.id = :id")
 	Optional<Payment> findByIdWithOrdersAndClients(@Param("id") Long id);
 }
