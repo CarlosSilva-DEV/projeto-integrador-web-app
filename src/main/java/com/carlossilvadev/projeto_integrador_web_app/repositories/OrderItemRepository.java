1278b33b36936691dc650fa8597d8ba31e6b65a0
Carlos Silva <cd160204@gmail.com>
2026-04-22 15:27:33 -0300

    refactor: remove método existsByProductId de OrderItemRepository
    este commit remove o método inutilizado existsByProductId de OrderItemRepository

--- src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/OrderItemRepository.java
+++ src/main/java/com/carlossilvadev/projeto_integrador_web_app/repositories/OrderItemRepository.java
@@ -1,7 +1,5 @@
 package com.carlossilvadev.projeto_integrador_web_app.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.carlossilvadev.projeto_integrador_web_app.entities.OrderItem;
@@ -9,7 +7,4 @@ import com.carlossilvadev.projeto_integrador_web_app.entities.pk.OrderItemPK;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
-	
-	@Query("SELECT COUNT(oi) > 0 FROM OrderItem oi WHERE oi.id.product.id = :productId")
-	boolean existsByProductId(@Param("productId") Long productId);
 }
